5f9ea0942c99782cacaa3953b39b13f020fff882
ninja25538 <ninja_william@hotmail.com>
2016-08-30 13:50:09 -0400

    Update quiz.js

--- quiz.js
+++ quiz.js
@@ -1,6 +1,13 @@
 var ready = confirm("Ready for the quiz?");
 if (ready == true){
   alert("Alrighty then, let's begin!");
+  alert("Note, answers are case-sensitive, sorry, and please use one word answers.");
+  var q1 = prompt("First question, what do the main characters travel through?");
+
+if(q1 == "time"){
+  alert("COOOOOOOOOOORECT!");
+}
+
 } else if (ready == false){
   alert("Ok then, why don't you go study some more.");
   window.location.href = "about";
